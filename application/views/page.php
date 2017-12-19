25fd1f57cb27dcbc50e6782d066089e075d6eb33
ChrisAsi <ChrisAsipyan@gmail.com>
2017-12-18 22:03:03 -0800

    Setup main page layout, menu, and added basic required frontend libraries

---
+++ application/views/page.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="assets/application/favicon.ico">
+
+    <title>WeightChallenge</title>
+
+    <link href="assets/bootstrap/ver_4.0.0-beta-2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/application/css/global.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="#">WeightChallenge</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <main role="main" class="container"><?=$body?></main>
+    
+    <script src="assets/jquery/ver_3.2.1-slim/jquery.slim.min.js"></script>
+    <script src="assets/popper/ver_2017/popper.min.js"></script>
+    <script src="assets/bootstrap/ver_4.0.0-beta-2/js/bootstrap.min.js"></script>
+  </body>
+</html>
